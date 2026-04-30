321346faab22b5c421c3f8276bedd26d06e11b42
RofLeR8 <mister.kabalin@bk.ru>
2026-04-30 10:14:53 +0300

    merge conflict revisions

---
+++ src/migration/versions/26e91c10241c_merge_conflict.py
@@ -0,0 +1,28 @@
+"""merge conflict
+
+Revision ID: 26e91c10241c
+Revises: 20260430_add_group_chat_photo, eeab2498aa50
+Create Date: 2026-04-30 09:55:39.845879
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '26e91c10241c'
+down_revision: Union[str, Sequence[str], None] = ('20260430_add_group_chat_photo', 'eeab2498aa50')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
